bbaa1e13966d05d33f0e4ece9c4d3ec83cd97512
Saptarshi Chakraborty <csaptarshi26@gmail.com>
2022-06-22 13:28:46 +0530

    config added

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,11 @@ dotenv.config();
 connectDb();
 const app = express();
 
-if(process.env.NODE_ENV === "development"){
+if (process.env.NODE_ENV === "development") {
   app.use(morgan('dev'))
 }
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('API is Running '))
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -28,8 +27,18 @@ app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+
+  app.get('/', (req, res) => res.send('API is Running '))
+}
 
 app.use(notFound)
 app.use(errorHandler)
